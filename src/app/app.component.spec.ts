dd64264df6a5c428f178a24ab84233c7b811291f
Adrián Fernández <97693921+adrianfernandezalv@users.noreply.github.com>
2024-08-04 21:26:27 +0200

    Create app project (#2)

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+
+describe("AppComponent", () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create the app", () => {
+    expect(component).toBeTruthy();
+  });
+});
